1dbf528e18be09875c419c17b5376ebab7e52318
Dzmitry Bryt <bdv212@yandex.ru>
2020-03-24 17:48:27 +0300

    feat: add switches for for navigation menu, add black screens for phones in slider sections, add slider

---
+++ script.js
@@ -0,0 +1,67 @@
+// nav menu
+const navList = document.querySelector('.nav__list');
+
+navList.addEventListener('click', event => {
+  navList
+    .querySelectorAll('.nav__link')
+    .forEach(el => el.classList.remove('nav__link_active'));
+  if (event.target.classList.contains('nav__link')) {
+    event.target.classList.add('nav__link_active');
+  }
+});
+
+//phones black screen
+const phoneVertical = document.querySelector('.slider__phoneImg-vertical');
+const phoneHorizontal = document.querySelector('.slider__phoneImg-horizontal');
+const phoneVerticalBlackScreen = document.querySelector(
+  '.phoneImg-vertical__blackScreen'
+);
+const phoneHorizontalBlackScreen = document.querySelector(
+  '.phoneImg-horizontal__blackScreen'
+);
+
+phoneVertical.onclick = function() {
+  const style = window.getComputedStyle(phoneVerticalBlackScreen);
+  const { display } = style;
+  console.log(style.display);
+  if (display === 'none') {
+    phoneVerticalBlackScreen.style.display = 'block';
+  } else if (display === 'block') {
+    phoneVerticalBlackScreen.style.display = 'none';
+  }
+};
+
+phoneHorizontal.onclick = function() {
+  const style = window.getComputedStyle(phoneHorizontalBlackScreen);
+  const { display } = style;
+  if (display === 'none') {
+    phoneHorizontalBlackScreen.style.display = 'block';
+  } else if (display === 'block') {
+    phoneHorizontalBlackScreen.style.display = 'none';
+  }
+};
+
+//slider 
+const buttonLeft = document.querySelector('.slider__button-left');
+const buttonRight = document.querySelector('.slider__button-right');
+let slider = document.querySelectorAll('.slider');
+let i = 0;
+
+buttonLeft.onclick = function() {
+  slider[i].style.display = 'none';
+  i++;
+  if (i >= slider.length) {
+    i = 0;
+  }
+  slider[i].style.display = 'flex';
+};
+
+buttonRight.onclick = function() {
+  slider[i].style.display = 'none';
+  i++;
+  if (i >= slider.length) {
+    i = 0;
+  }
+  slider[i].style.display = 'flex';
+};
+
